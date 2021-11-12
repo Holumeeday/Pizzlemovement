b12a4a26b38230277bac4387ddfb36adb8defd48
Ajayi Olumide <60358439+Holumeeday@users.noreply.github.com>
2021-11-12 06:55:48 +0100

    again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { MusicComponent } from './music/music.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'gallery' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent }, 
   {path: 'music', component: MusicComponent},
   {path: 'gallery', component: GalleryComponent}  
